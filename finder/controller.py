10404255a1f0698bf52d054614aa48f7085f6194
Suraj Thapar <surajthapar.in@gmail.com>
2020-07-07 03:56:39 +0530

    Search book + Async implementation of Author API

--- finder/controller.py
+++ finder/controller.py
@@ -1,6 +1,34 @@
+import aiohttp
+import asyncio
 from scout import Scout
+from .exceptions import UnresponsiveAuthorAPI
 
 
 def search_book_index(query, limit, database):
     sc = Scout(database)
-    return sc.search(query, limit)
+    return [{
+        "id": r['id'],
+        "summary": r['summary'],
+        "query": query
+    } for r in sc.search(query, limit)]
+
+
+async def get_authors(ids):
+
+    async def hit_author(id):
+        endpoint = "https://ie4djxzt8j.execute-api.eu-west-1.amazonaws.com/coding"
+
+        async with aiohttp.ClientSession() as session:
+            async with session.post(endpoint,
+                                    json={"book_id": id},
+                                    ssl=False
+                                    ) as response:
+                if response.status == 200:
+                    js = await response.json()
+                    return (id, js['author'])
+                else:
+                    raise UnresponsiveAuthorAPI(
+                                f"Response Code : {response.status}"
+                            )
+
+    return await asyncio.gather(*[hit_author(id) for id in ids])
