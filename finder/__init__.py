c15b55f20cb5e8c4b4d77b85e2c76b73d7a45a66
Suraj Thapar <surajthapar.in@gmail.com>
2020-07-07 00:14:25 +0530

    Main wsgi app

---
+++ finder/__init__.py
@@ -0,0 +1,12 @@
+from pyramid.config import Configurator
+
+
+def main(global_config, **settings):
+    """ Pyramid WSGI application.
+    """
+    with Configurator(settings=settings) as config:
+
+        # Project : Books
+        config.scan('books')
+
+    return config.make_wsgi_app()
