cedb62436835f3ff4ed98e92ce8ec7b3f64f8101
Suraj Thapar <surajthapar.in@gmail.com>
2020-07-07 15:01:13 +0530

    dostring : controller / get_authors

--- finder/controller.py
+++ finder/controller.py
@@ -1,10 +1,26 @@
 import aiohttp
 import asyncio
+from typing import List, Dict
 from scout import Scout
 from .exceptions import UnresponsiveAuthorAPI
 
 
-def search_book_index(query, limit, database):
+def search_book_index(query: str,
+                      limit: int,
+                      database: str
+                      ) -> List[Dict[str, str]]:
+    """Search book index uses Scout search engine to fetch results.
+
+    :param query: User input query terms.
+    :type query: str
+    :param limit: Maximum results to fetch.
+    :type limit: int
+    :param database: Scout index SQLite3 database file.
+    :type database: str
+    :return: List of results with Book's id, summary and query.
+    :rtype: List[Dict[str, str]]
+
+    """
     sc = Scout(database)
     return [{
         "id": r['id'],
@@ -13,8 +29,20 @@ def search_book_index(query, limit, database):
     } for r in sc.search(query, limit)]
 
 
-async def get_authors(ids):
+async def get_authors(ids: List[str]) -> Dict[str, str]:
+    """Get authors asynchronously fetches Book Authors from Author API.
 
+    Author API is hit in async manner. This reduces the load time by
+    5 folds in 10 results.
+
+    TODO : Caching implementation to reduce server load.
+
+    :param ids: List of book id.
+    :type ids: List[str]
+    :return: Dictionary map of (id, author).
+    :rtype: Dict[str, str]
+
+    """
     async def hit_author(id):
         endpoint = "https://ie4djxzt8j.execute-api.eu-west-1.amazonaws.com/coding"
 
