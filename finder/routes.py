ac2b6d516e2c61e0ead6c3c3adc0bd9bb25a93b0
Suraj Thapar <surajthapar.in@gmail.com>
2020-07-07 15:01:23 +0530

    dostring : routes

--- finder/routes.py
+++ finder/routes.py
@@ -1,2 +1,11 @@
+"""List of API urls
+
+| Route Name  | URL      |
+--------------------------
+| book_search | /search/ |
+
+"""
+
+
 def includeme(config):
     config.add_route('book_search', '/search/')
